cfb1465e6cab6623dd78896fd90083d3b34572a0
TGFo <102477641+TGFo@users.noreply.github.com>
2024-05-01 05:44:45 +0200

    beganRough

---
+++ Assets/Scripts/BoardManager.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoardManager : MonoBehaviour
+{
+    public GameObject pointPrefab;
+    BoardPoint[,] points = new BoardPoint[8,8];
+    public Vector3 startPoint;
+    public float lengthWidth;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
