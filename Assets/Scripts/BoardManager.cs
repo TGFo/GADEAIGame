1ece55d83447ecaa41d2a0f811e35ca165e7cb25
TGFo <102477641+TGFo@users.noreply.github.com>
2024-05-03 12:06:31 +0200

    ManyThings

--- Assets/Scripts/BoardManager.cs
+++ Assets/Scripts/BoardManager.cs
@@ -6,17 +6,68 @@ public class BoardManager : MonoBehaviour
 {
     public GameObject pointPrefab;
     BoardPoint[,] points = new BoardPoint[8,8];
+    GameObject[] player1Pieces = new GameObject[4];
+    GameObject[] player2Pieces = new GameObject[4];
     public Vector3 startPoint;
+    public Vector3 placePoint;
     public float lengthWidth;
     // Start is called before the first frame update
     void Start()
     {
         
     }
-
+    private void Awake()
+    {
+        GenerateBoard();
+    }
     // Update is called once per frame
     void Update()
     {
         
+    }
+    public void GenerateBoard()
+    {
+        string pointName;
+        GameObject pointObject;
+        for (int i = 0; i < 8; i++)
+        {
+            for (int j = 0; j < 8; j++)
+            {
+                pointObject = Instantiate(pointPrefab);
+                points[i, j] = pointObject.GetComponent<BoardPoint>();
+                pointName = i + "_" + j;
+                pointObject.name = pointName;
+                pointObject.transform.position = placePoint;
+                placePoint.x = placePoint.x + (lengthWidth / 8);
+                if(i == 0)
+                {
+                    points[i, j].isScorePoint = true;
+                }
+                else
+                {
+                    points[i, j].isScorePoint = false;
+                }
+            }
+            placePoint.x = startPoint.x;
+            placePoint.z = placePoint.z - (lengthWidth / 8);
+        }
+    }
+    public GameObject[] PlacePlayer1Pieces(GameObject p1Piece)
+    {
+        for(int i = 0; i < 4; i++) 
+        {
+            player1Pieces[i] = Instantiate(p1Piece);
+            player1Pieces[i].transform.position = points[7 - i, 0].transform.position;
+        }
+        return player1Pieces;
+    }
+    public GameObject[] PlacePlayer2Pieces(GameObject p2Piece)
+    {
+        for (int i = 0; i < 4; i++)
+        {
+            player2Pieces[i] = Instantiate(p2Piece);
+            player2Pieces[i].transform.position = points[7 - i, 7].transform.position;
+        }
+        return player2Pieces;
     }
 }
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,22 +1,125 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    enum PointState
-    {
+    public GameObject[] player1Pieces;
+    public GameObject[] player2Pieces;
+    public BoardManager boardManager;
+    public GameObject player1Prefab;
+    public GameObject player2Prefab;
 
-    }
+    public GameObject modeSelectPanel;
+
+    public TMP_Text currentTurnText;
+
+    public int turn = 0;
+    public bool isP1Turn = true;
+    public int blueScore = 0;
+    public int redScore = 0;
+
+    public MovingPoint currentlySelectedPawn;
+    public MovingPoint previouslySelectedPawn;
+
+    public static GameManager instance;
     // Start is called before the first frame update
+    private void Awake()
+    {
+        if(instance == null)
+        {
+            instance = this;
+        }else
+        {
+            Destroy(gameObject);
+        }
+    }
     void Start()
     {
-        
+        DisplayCurrentTurn();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        if(Input.GetMouseButtonDown(0))
+        {
+            if(Physics.Raycast(ray, out RaycastHit hit, 10000, 1, QueryTriggerInteraction.Ignore))
+            {
+                if(hit.transform.TryGetComponent<MovingPoint>(out MovingPoint point))
+                {
+                    Debug.Log(hit.transform.name);
+                    HighlightPossibleMoves(point);
+                }
+            }
+        }
+    }
+    public void PlayerVsPlayerBtn()
+    {
+        player1Pieces = boardManager.PlacePlayer1Pieces(player1Prefab);
+        player2Pieces = boardManager.PlacePlayer2Pieces(player2Prefab);
+        modeSelectPanel.SetActive(false);
+    }
+    public void SelectPawn(MovingPoint point)
+    {
+        if(previouslySelectedPawn == null)
+        {
+            currentlySelectedPawn = point;
+            previouslySelectedPawn = point;
+            currentlySelectedPawn.isSelected = true;
+            showSelectedMoves();
+            return;
+        }
+        previouslySelectedPawn = currentlySelectedPawn;
+        currentlySelectedPawn = point;
+        previouslySelectedPawn.isSelected = false;
+        currentlySelectedPawn.isSelected = true;
+        showSelectedMoves();
+    }
+    public void DeselectPawn()
+    {
+        if(previouslySelectedPawn != null)
+        {
+            previouslySelectedPawn.isSelected = false;
+            currentlySelectedPawn.isSelected = false;
+            showSelectedMoves();
+        }
+        currentlySelectedPawn = null;
+        previouslySelectedPawn = null;
+    }
+    public void HighlightPossibleMoves(MovingPoint point)
+    {
+        if(point.isP1Piece == isP1Turn)
+        {
+            SelectPawn(point);
+        }else
+        {
+            DeselectPawn();
+        }
+    }
+    public void showSelectedMoves()
+    {
+        currentlySelectedPawn.ShowMovePoints();
+        previouslySelectedPawn.ShowMovePoints();
+    }
+    public void EndTurn()
+    {
+        isP1Turn = !isP1Turn;
+        DeselectPawn();
+        DisplayCurrentTurn();
+        turn++;
+    }
+    public void DisplayCurrentTurn()
+    {
+        if (isP1Turn)
+        {
+            currentTurnText.text = "Blue turn";
+        }
+        else
+        {
+            currentTurnText.text = "Red turn";
+        }
     }
 }
